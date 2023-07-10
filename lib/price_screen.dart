ba975382f78d97f030e97f56ab256622ae962ac9
yyankov <yavor.yankov@lom.com>
2023-07-10 10:49:59 +0100

    Added currencies dropdown

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,11 +1,25 @@
+// ignore_for_file: library_private_types_in_public_api, prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+
+import 'package:crypto_ticker/coin_data.dart';
 import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
+  const PriceScreen({super.key});
+
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  List<DropdownMenuItem<String>> currenciesDropdownItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    populateCurrencyDropdown();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,10 +56,29 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: currenciesDropdownItems,
+              onChanged: (value) => {
+                setState(
+                  () => selectedCurrency = value!,
+                )
+              },
+            ),
           ),
         ],
       ),
     );
+  }
+
+  void populateCurrencyDropdown() {
+    for (var currency in currenciesList) {
+      currenciesDropdownItems.add(
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        ),
+      );
+    }
   }
 }
